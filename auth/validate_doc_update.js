eb2a07168e10c26c55a4a6a75e910fd856293c66
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-19 11:47:34 +0200

    First draft on document validation.
    - docs must have doc.type set to "Gemarkung"
    - everyone has read-access, users may change documents

---
+++ auth/validate_doc_update.js
@@ -0,0 +1,10 @@
+function(newDoc, oldDoc, userCtx, secObj) {
+// make your db read-only for everyone and grant
+// write-access for normal documents to all users.
+  if (userCtx.roles.indexOf("user")==-1)
+    throw({forbidden: 'Database is read-only for non-members!'});
+// request some self-explanatory power
+  let types=["Gemarkung"];
+  if (types.indexOf(newDoc.type)==-1)
+    throw({invalid: "doc.type must be set and one of "+types.join(", ")});
+}
