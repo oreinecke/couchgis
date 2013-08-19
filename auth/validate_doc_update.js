45a38ec3b56177a6a3bcfd75876f887689caf5ef
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-19 16:55:52 +0200

    Added type 'FlurStk' to document validation.

--- auth/validate_doc_update.js
+++ auth/validate_doc_update.js
@@ -4,7 +4,7 @@ function(newDoc, oldDoc, userCtx, secObj) {
   if (userCtx.roles.indexOf("user")==-1)
     throw({forbidden: 'Database is read-only for non-members!'});
 // request some self-explanatory power
-  let types=["Gemarkung"];
+  let types=["Gemarkung", "FlurStk"];
   if (types.indexOf(newDoc.type)==-1)
     throw({invalid: "doc.type must be set and one of "+types.join(", ")});
 }
