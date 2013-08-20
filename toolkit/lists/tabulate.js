fbce024518263dc3fbd4318a747247119abae6f2
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-20 12:47:08 +0200

    Installed tabulate from COBRA's 'meta4re' design doc.
    Command separators have been added since this is good javascript practise. In
    the old days at COBRA they did not really care about this bad habit.

---
+++ toolkit/lists/tabulate.js
@@ -0,0 +1,41 @@
+// tabulates over members if include_docs is given.
+// This was originally part of COBRA's 'meta4re' design doc.
+
+function(head, req) {
+  start({'headers':{'Content-Type':'text/plain'}});
+  var tabs = [];
+  var cuts = {};
+  for (query in req.query)
+    if (req.query[query]=="")
+      tabs=tabs.concat(query.split(':'));
+    else
+      cuts[query]=req.query[query];
+  while(row=getRow()) {
+    if (!row.doc) throw({invalid: "No document supplied to view"});
+    for (cut in cuts) {
+      fields=cut.split('.');
+      var val=row.doc;
+      for (field in fields) {
+        val=val[fields[field]];
+        if (!val) break;
+      }
+      if (val && val!=cuts[cut]) {
+        row=null;
+        break;
+      }
+    }
+    fields=null;
+    if (!row) continue;
+    for (tab in tabs) {
+      if (fields) send(' ');
+      fields=tabs[tab].split('.');
+      var val=row.doc;
+      for (field in fields) {
+        val=val[fields[field]];
+        if (!val) break;
+      }
+      if (val) send(JSON.stringify(val).replace(/^"|"$/g,''));
+    }
+    send("\n");
+  }
+}
