30ed066c8e562c77392765c9eb338d1cb1ba61a0
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-20 11:32:16 +0200

    Fixed two issues in document update validation.
    1) only 'unauthorized' and 'forbidden' are accepted as throws
    2) restriction on doc structure does not apply for deleted docs

--- auth/validate_doc_update.js
+++ auth/validate_doc_update.js
@@ -2,9 +2,9 @@ function(newDoc, oldDoc, userCtx, secObj) {
 // make your db read-only for everyone and grant
 // write-access for normal documents to all users.
   if (userCtx.roles.indexOf("user")==-1)
-    throw({forbidden: 'Database is read-only for non-members!'});
+    throw({unauthorized: 'Database is read-only for non-members!'});
 // request some self-explanatory power
-  let types=["Gemarkung", "FlurStk"];
-  if (types.indexOf(newDoc.type)==-1)
-    throw({invalid: "doc.type must be set and one of "+types.join(", ")});
+  var types=["Gemarkung", "FlurStk"];
+  if (!newDoc._deleted && types.indexOf(newDoc.type)==-1)
+    throw({forbidden: "doc.type must be set and one of "+types.join("/")});
 }
