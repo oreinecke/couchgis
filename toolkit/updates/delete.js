ce6a115f88b92c84852447332a23523883691542
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-21 13:39:51 +0200

    Included newlines in the response of /_update/delete.

--- toolkit/updates/delete.js
+++ toolkit/updates/delete.js
@@ -3,9 +3,9 @@
 
 function(doc, req) {
   if (req.userCtx.roles.indexOf("_admin")==-1) 
-    return[null, 'Only admins are allowed to use /_update/delete!'];
+    return[null, 'Only admins are allowed to use /_update/delete!\n'];
   if (!doc)
-    return[null, 'Document must be specified and existing!'];
+    return[null, 'Document must be specified and existing!\n'];
   doc._deleted=true;
-  return[doc, 'Deleted '+doc._id];
+  return[doc, 'Deleted '+doc._id+'\n'];
 }
