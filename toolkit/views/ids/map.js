9b8689101918732abef210cf032f455eec2b615c
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-22 12:39:45 +0200

    Installed ids into views.
    Ids crawls recursively through the document and emits an {_id:obj._id} for
    every _id field it finds. Identical ids aren't emitted twice. This way we can
    at least link at one level. Two levels or more should be done by the client if
    we ever need something like that. This isn't a directed graph.

---
+++ toolkit/views/ids/map.js
@@ -0,0 +1,15 @@
+// Searches document recursively for _id's and emits links.
+
+function(doc) {
+  var ids={};
+  // erica only pushes anonymous functions
+  var collect_ids=function(obj) {
+    if (typeof(obj)=="object" && typeof(obj._id)=="string")
+      ids[obj._id]=null;
+    if (typeof(obj)=="array" || typeof(obj)=="object")
+      for (prop in obj)
+        collect_ids(obj[prop]);
+  }
+  collect_ids(doc);
+  for (id in ids) emit(null, {_id:id});
+}
