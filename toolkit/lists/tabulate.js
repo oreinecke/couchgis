c80381298ef90a5a00dc3ea444e626557b7cbef4
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-27 13:37:28 +0200

    Unified _list/substitute and _list/tabulate.
    As long as we don't need to tabulate over multiple keys per document, we might
    as well use one list that handles linked docs and key/value pairs. The best
    part is that views don't look esoteric anymore. The just read
    _list/tabulate/<what>?<query args>. Also I excluded query arguments of _view/
    to be excluded from parsing. This fixes most of the errors from before.

--- toolkit/lists/tabulate.js
+++ toolkit/lists/tabulate.js
@@ -1,4 +1,6 @@
-// Tabulates over members of selected rows. This was
+// Tabulates over members of selected rows and
+// transcends into linked documents' structure
+// if indicated by the _id member. This was
 // originally part of COBRA's 'meta4re' design doc.
 
 function(head, req) {
@@ -9,32 +11,48 @@ function(head, req) {
     if (req.query[query]=="")
       tabs=tabs.concat(query.split(':'));
     else
-      cuts[query]=req.query[query];
-  while(row=getRow()) {
-    for (cut in cuts) {
-      fields=cut.split('.');
-      var val=row;
-      for (f=0;f<fields.length;f++) {
-        val=val[fields[f]];
-        if (!val) break;
+      // ignore _view API query arguments
+      if (query.match(/^(key\.|(val|doc)($|\.))/)) cuts[query]=req.query[query];
+  var row={}, doc={}, last_row=null;
+  while(row) {
+    if (row) row=getRow();
+    if (!row || last_row && last_row.id!=row.id) {
+      for (cut in cuts) {
+        fields=cut.split('.');
+        var val=last_row;
+        for (f=0;f<fields.length;f++) {
+          val=val[fields[f]];
+          if (!val) break;
+          // dive into linked docs if the are available
+          if (val._id && doc[val._id]) val=doc[val._id];
+        }
+        if (val && val!=cuts[cut]) {
+          last_row=null;
+          break;
+        }
       }
-      if (val && val!=cuts[cut]) {
-        row=null;
-        break;
+      if (!last_row) continue;
+      fields=null;
+      for (t=0;t<tabs.length;t++) {
+        if (fields) send(' ');
+        fields=tabs[t].split('.');
+        var val=last_row;
+        for (f=0;f<fields.length;f++) {
+          val=val[fields[f]];
+          if (!val) break;
+          // dive into linked docs if the are available
+          if (val._id && doc[val._id]) val=doc[val._id];
+        }
+        if (val) send(JSON.stringify(val).replace(/^"|"$/g,''));
       }
+      send("\n");
+      doc={};
     }
-    fields=null;
     if (!row) continue;
-    for (t=0;t<tabs.length;t++) {
-      if (fields) send(' ');
-      fields=tabs[t].split('.');
-      var val=row;
-      for (f=0;f<fields.length;f++) {
-        val=val[fields[f]];
-        if (!val) break;
-      }
-      if (val) send(JSON.stringify(val).replace(/^"|"$/g,''));
-    }
-    send("\n");
+    last_row=row;
+    // store linked docs for later use
+    if (row.doc) doc[row.doc._id]=row.doc;
+    // always start from the unlinked doc
+    if (doc[row.id]) last_row.doc=doc[row.id];
   }
 }
