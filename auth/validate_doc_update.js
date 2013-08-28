4aeeb7319c072754a691af7fe2e50aa069766d1d
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-28 16:44:38 +0200

    Imposed restrictions links to other documents.
    1) type must be indicated such as '{.., Gemarkung:{_id:"abc"}, ..}, this means
      only known types are allowed,
    2) _id is the only allowed property and
    3) it must be a string
    All of these are useful but I also might be able to live without all these
    restrictions. The reason for 3) is that all doc._id's must be strings, so links
    to other docs should as well. 2) is meant to avert the user from stuffing extra
    information into links that do not belong there. 1) would allow me to display
    the right link/graphic button without an extra httpget.

--- auth/validate_doc_update.js
+++ auth/validate_doc_update.js
@@ -3,8 +3,30 @@ function(newDoc, oldDoc, userCtx, secObj) {
 // write-access for normal documents to all users.
   if (userCtx.roles.indexOf("user")==-1)
     throw({unauthorized: 'Database is read-only for non-members!'});
+// don't apply any design restrictions to deleted docs
+  if (newDoc._deleted) return;
 // request some self-explanatory power
   var types=["Gemarkung", "FlurStk", "Adresse"];
-  if (!newDoc._deleted && types.indexOf(newDoc.type)==-1)
+  if (types.indexOf(newDoc.type)==-1)
     throw({forbidden: "doc.type must be set and one of "+types.join("/")});
+// linked documents should look like "link:{_id:id}"
+// and 'link' itself has to be on of 'types' because
+// we have to know what we're linking to
+  var inspect_ids=function(obj) {
+    if (typeof(obj)!="array" && typeof(obj)!="object")
+      return;
+    for (key in obj) {
+      var prop=obj[key];
+      // because typeof(null)=="object"
+      if (!prop) continue;
+      if (obj!==newDoc && obj._id!==undefined && key!="_id")
+        throw({forbidden: "Link objects must consist of _id only"});
+      if (typeof(prop)=="object" && prop._id!==undefined) {
+        if (typeof(prop._id)!="string") throw({forbidden: "Linked document ID "+prop._id+" must be a string"});
+        if (types.indexOf(key)==-1) throw({forbidden: "Property "+key+" must indicate known type"});
+      }
+      inspect_ids(prop);
+    }
+  };
+  inspect_ids(newDoc);
 }
