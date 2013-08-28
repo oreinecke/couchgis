df5a20cb3d6f85585e81d20ebba211f4fd0a4215
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-08-28 15:45:41 +0200

    Improved reduce function of _view/types-fields.
    If group_level is set to one, the new reduce function yields a list of every
    field that was ever used in this specific type of document, as given by key[0].
    This should make it easy to achieve some kind of 'document-driven design':
    instead of specifying every freaking form field beforehand, we just look into
    what's already being used and create a form from that. And perhaps leave one or
    two open fields if the user wants to define something new.

--- toolkit/views/types-fields/reduce.js
+++ toolkit/views/types-fields/reduce.js
@@ -1,1 +1,16 @@
-_count
+// If queried with group_level=1, it returns the union of
+// all fields ever used for one specific document type.
+
+function(keys, values, rereduce) {
+  var result=[];
+  var K=(rereduce?values.length:keys.length);
+  for (k=0;k<K;k++) {
+    fields=(rereduce?values[k]:keys[k][0][1].split('/'));
+    for (f=0;f<fields.length;f++) {
+      var field=fields[f];
+      if (result.indexOf(field)==-1) result.push(field);
+    }
+  }
+  return result.sort();
+}
+
