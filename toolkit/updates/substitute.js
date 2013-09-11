06ff83dd219410f644d431208fe23dace4ddfac5
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-09-11 14:34:31 +0200

    Installed substitute into updates.
    The essence of NoSQL is not to use reference tables at all. Whenever a
    reference table is introduced, the raw data ceases to be self-explanatory and
    gets unreadable. One "good thing" about reference tables however, is that we
    have to change the referenced value in one place and it then applies to all
    documents. The same thing can be achieved by a sed-style edit of the entire
    JSON structure as given by /_updates/substitute. The syntax reads
      /_updates/substitute/<doc>?/<pattern>/<flags>=<new string>&<...>

---
+++ toolkit/updates/substitute.js
@@ -0,0 +1,37 @@
+// Applies regexp substitution to stringified doc. Syntax 
+// reads /substitute/<doc>?/<pattern>/<flags>=<replacement>.
+
+function(doc, req) {
+  if (req.userCtx.roles.indexOf("_admin")==-1) 
+    return[null, 'Only admins are allowed to use /_update/substitute!\n'];
+  if (!doc)
+    return[null, 'Document must be specified and existing!\n'];
+  // keep special members and exclude from substitution
+  var special={_id:doc._id,_rev:doc._rev};
+  delete doc._id;
+  delete doc._rev;
+  doc=JSON.stringify(doc);
+  var changed=false;
+  for (q in req.query) {
+    var flags=q.match(/\/[^/]*$/);
+    var pattern;
+    try {
+      pattern=new RegExp(q.substring(1,flags.index), flags[0].substring(1));
+    } catch(err) {
+      return[null, "RegExp error: "+err.message+'\n'];
+    }
+    var oldDoc=doc
+    doc=doc.replace(pattern, req.query[q]);
+    changed|=(oldDoc!=doc);
+  }
+  try {
+    doc=JSON.parse(doc);
+  } catch(err) {
+    return[null, "Invalid JSON by bad substitution\n"];
+  }
+  doc._id=special._id;
+  doc._rev=special._rev;
+  // avoid new revisions if no apparent changes
+  if (changed) return[doc, 'Substituted '+doc._id+'\n'];
+  else return[null, 'Unchanged '+doc._id+'\n'];
+}
