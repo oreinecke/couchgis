60bb4227fd60a824c0ceed78d3db1a50f175e91a
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-09-25 13:33:19 +0200

    Removed all /^GeoJSON/ from fields listing.

--- toolkit/views/types-fields/map.js
+++ toolkit/views/types-fields/map.js
@@ -1,11 +1,12 @@
 // Lists type and then available fields. Fields are a simple
 // string "field1/field2/..." because fields have arbitrary
 // order and are henceforth useless to group_level.
+// Specials, 'type', 'GeoJSON', and 'GeoJSON_clone' are ignored.
 
 function(doc) {
   key = [];
   for (var field in doc)
-    if (field!="type"&&field[0]!='_');
+    if (field!="type" && field.search(/^GeoJSON/)==-1 && field[0]!='_')
       key.push(field);
   emit([doc.type, key.join('/')]);
 }
