72f930de93eed8026873b821b926f09f6bf33295
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-09-26 14:58:43 +0200

    Scratched the geojson reduce function.
    Due to the size limit of reduce's return value (900 bytes or something) I am
    unable to use it to collect docs in it that share a GeoJSON object. I might
    turn it off by posting to /_config but then I'd have to do so in every local
    installation (and assume admin party etc. NO WAY!!!!).

--- testapp/views/geojson/map.js
+++ testapp/views/geojson/map.js
@@ -24,14 +24,14 @@ function(doc) {
       }
     }
     update_bbox(GeoJSON);
-    emit(doc._id, {GeoJSON: GeoJSON});
+    emit(id, {GeoJSON: GeoJSON});
   // abort if no GeoJSON is set at all
   };
   var properties={_id:doc._id, type:doc.type};
   for (field in doc) {
     // fields with lowecase letters are english and kind of 'internal'
     if (field.search(/^[A-Z]/)==-1) continue;
-    if (field.search("GeoJSON")==-1) continue;
+    if (field.search("GeoJSON")!=-1) continue;
     properties[field]=doc[field];
   }
   emit(id, {doc:properties});
