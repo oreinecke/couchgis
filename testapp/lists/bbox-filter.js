4893cc6c91550d065257dbf5f3d964494e73ed8b
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-10-01 16:23:00 +0200

    Introduced sort_index parameter to spacial indexing.
    If sort_index is specified as a number in the request body, it will be used to
    decide if rows are sorted according to size after a certain index. The idea
    behind this goes as follows:
    * client obtains an ordered geometry list [a,b..h] and queries
      _lists/bbox-filter with {keys:[a,b..h], sort_index:0, limit:3, bbox:[..]}
    * filter returns [a,b,e] that fall inside bbox. Every row needs to be checked.
    * Client puts [a,b,e] in front of the list for the next query.
    * So the list reads [a,b,e,c,d,f..h] and sort_index is 3 because there are three
      unsorted items and the rest is still in descending order.
    * If filter is queried with the same bbox, it only has to check til f.
    * If queried with an infinite bbox, it only checks up to d and returns [a,b,c].
    * The Client now has [a,b,e] as old prefix and removes a,b from it. c is
      removed from the sorted suffix. e remains in the old prefix. This yields
      [a,b,c][e][d,f..h]. Old and new prefix are merged, sort_index becomes 4.

--- testapp/lists/bbox-filter.js
+++ testapp/lists/bbox-filter.js
@@ -10,26 +10,34 @@ function(head, req) {
     bbox=[-Infinity,-Infinity,Infinity,Infinity];
   // we need to limit output and server load
   var limit=body.limit;
-  if (limit==null) limit=Infinity;
+  if (typeof(limit)!="number") limit=Infinity;
   // and allow to filter types
   var types=body.types;
-  if (typeof(types)!="object")
+  if (typeof(types)!="object" || typeof(types.indexOf)!="function")
     types=null;
+  // We expect keys have random items first (that have been returned at the
+  // last query and are likely to be returned again) followes by remaining
+  // items ordered by decreasing size. If the client provides this 'sort'
+  // threshold, we may safely break out of the loop!
+  var unsorted=body.sort_index;
+  if (typeof(sort_index)!="number") sort_index=Infinity;
   var items=[];
   var row={}, last_row=null, docs=[], GeoJSON, size;
   while (row) {
     if (row) row=getRow();
     if (last_row && (row==null || last_row.key!=row.key)) {
-      if ( items.length!=limit || size > items[items.length-1] )
-      if ( docs.length>0 )
-      if ( bbox[0]<=GeoJSON.bbox[2] && GeoJSON.bbox[0]<=bbox[2] && 
-           bbox[1]<=GeoJSON.bbox[3] && GeoJSON.bbox[1]<=bbox[3]) {
+      if ( items.length==limit && size <= items[items.length-1]) {
+        // no need to look further if only smaller items are expected
+        if (!unsorted) break;
+      } else if ( docs.length > 0 &&
+                  bbox[0]<=GeoJSON.bbox[2] && GeoJSON.bbox[0]<=bbox[2] && 
+                  bbox[1]<=GeoJSON.bbox[3] && GeoJSON.bbox[1]<=bbox[3]) {
         var item={size:size,id:last_row.key,GeoJSON:GeoJSON,docs:docs};
         items.push(item);
-        // performance may be improved here
-        // with binary search or something
+        // performance may be improved here with binary search or something
         items=items.sort(function(a,b){return a.size<b.size;}).slice(0,limit);
       }
+      if (unsorted) unsorted--;
       docs=[];
     }
     if (!row) continue;
