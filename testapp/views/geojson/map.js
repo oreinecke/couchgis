95362bc56a2f6bd5f2047d814b32aac64adaee6e
Oscar Reinecke <oscar.reinecke@tu-dresden.de>
2013-10-02 13:11:21 +0200

    Moved bbox and size to CommonJS lib 'views/lib/utils'.
    I most certainly will need the stupid bbox calculation again for filtered
    replication and other views etc.

--- testapp/views/geojson/map.js
+++ testapp/views/geojson/map.js
@@ -1,35 +1,15 @@
 // Emits GeoJSON w/bbox and associated properties.
 
 function(doc) {
+  var utils=require('views/lib/utils');
   var id=doc["GeoJSON" in doc?"_id":"GeoJSON_clone"];
   // abort if no GeoJSON is set at all
   if (id==null) return;
   if (id===doc._id) {
 //  I need a copy that can be modified
     var GeoJSON=JSON.parse(JSON.stringify(doc.GeoJSON));
-    ;function update_bbox(obj) {
-      if (typeof(obj)!="object") return;
-      if (typeof(obj[0])=="number") {
-        if (GeoJSON.bbox===undefined)
-          GeoJSON.bbox=[obj[0],obj[1],obj[0],obj[1]];
-        var bbox=GeoJSON.bbox;
-        bbox[0]=(bbox[0]<obj[0])?bbox[0]:obj[0];
-        bbox[1]=(bbox[1]<obj[1])?bbox[1]:obj[1];
-        bbox[2]=(bbox[2]>obj[0])?bbox[2]:obj[0];
-        bbox[3]=(bbox[3]>obj[1])?bbox[3]:obj[1];
-      } else for (field in obj) {
-        if (["geometries", "coordinates", "features", "geometry"].indexOf(field)!=-1)
-          update_bbox(obj[field]);
-        if (field.search(/^[0-9]+$/)>=0)
-          update_bbox(obj[field]);
-      }
-    }
-    update_bbox(GeoJSON);
-    // I also decide on some kind of 'importance' here
-    // to choose what items to draw first. We use the
-    // bbox circumfence and we'll see how this goes.
-    var size=(GeoJSON.bbox[2]-GeoJSON.bbox[0])+(GeoJSON.bbox[3]-GeoJSON.bbox[1])
-    emit(id, {GeoJSON: GeoJSON, size: size});
+    GeoJSON.bbox=util.bbox(GeoJSON);
+    emit(id, {GeoJSON: GeoJSON, size: util.size(GeoJSON.bbox)});
   };
   var properties={_id:doc._id, type:doc.type};
   for (field in doc) {
---
+++ testapp/views/lib/utils.js
@@ -0,0 +1,29 @@
+// Dive into GeoJSON recursively and calculate a bounding box.
+
+exports.bbox=function(GeoJSON) {
+  var bbox=[Infinity,Infinity,-Infinity,-Infinity];
+  ;function update_bbox(obj) {
+    if (typeof(obj)!="object") return;
+    if (typeof(obj[0])=="number") {
+      bbox[0]=(bbox[0]<obj[0])?bbox[0]:obj[0];
+      bbox[1]=(bbox[1]<obj[1])?bbox[1]:obj[1];
+      bbox[2]=(bbox[2]>obj[0])?bbox[2]:obj[0];
+      bbox[3]=(bbox[3]>obj[1])?bbox[3]:obj[1];
+    } else for (field in obj) {
+      if (["geometries", "coordinates", "features", "geometry"].indexOf(field)!=-1)
+        update_bbox(obj[field]);
+      if (field.search(/^[0-9]+$/)>=0)
+        update_bbox(obj[field]);
+    }
+  }
+  update_bbox(GeoJSON);
+  return bbox;
+}
+
+// Also define some kind of 'importance' here to choose
+// what items to draw first. We use the bbox circumfence
+// and we'll see how this goes.
+
+exports.size=function(bbox) {
+  return (bbox[2]-bbox[0])+(bbox[3]-bbox[1]);
+}
