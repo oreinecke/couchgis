417378d711a7851ec9ac8e7db2aaece8ad6e32a4
Oscar Reinecke <reinecke@posteo.org>
2013-10-10 15:05:24 +0200

    Removed useless 'class' statements from DOM tree.
    I should have read the JQuery API more thoroughly in the first place.

--- testapp/_attachments/script/login_bar.js
+++ testapp/_attachments/script/login_bar.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
     success: function(resp) {
       if (resp.userCtx.name!==null || resp.userCtx.roles && resp.userCtx.roles.indexOf("_admin")>=0) {
         $("#logout").show();
-        href=$("#logout .username");
+        href=$("#logout a");
         // expect admin party if name==null
         href.text(resp.userCtx.name||"Administrator (keine Benutzer festgelegt)");
         // link to futon user doc
         if (resp.userCtx.name)
           href.attr({href:"/_utils/document.html?_users/org.couchdb.user:"+href.text()});
         else
-          $("#logout input").hide();
+          $("#logout :submit").hide();
         $("#login").hide();
-        $("#login .error").hide();
+        $("#login b").hide();
       } else {
         $("#login").show();
         $("#logout").hide();
@@ -23,13 +23,13 @@ $(document).ready(function() {
   $("#login").submit(function(e) {
     e.preventDefault();
     var login={
-      name:     $("#login .username").val(),
-      password: $("#login .password").val(),
+      name:     $("#login :text").val(),
+      password: $("#login :password").val(),
       success:function(resp) {
         log.success({userCtx:{name:this.name}});
       },
       error:function() {
-        $("#login .error").show();
+        $("#login b").show();
       }
     };
     $.couch.login(login);
