5573045a53c67e01488add0dc447d968f887ef29
Oscar Reinecke <reinecke@posteo.org>
2013-10-15 12:46:10 +0200

    Added utils/eachCoord.
    This is probably similar to jQuery.each() and allows me to run a function on
    every coordinate. Because I will be using this more often.

--- testapp/views/lib/utils.js
+++ testapp/views/lib/utils.js
@@ -1,24 +1,32 @@
-// Dive into GeoJSON recursively and calculate a bounding box.
+// Apply action to every GeoJSON coordinate.
+
+exports.eachCoord=function(GeoJSON, action) {
+  ;function apply_action(obj) {
+    if (typeof(obj)!="object") return;
+    if (typeof(obj[0])=="number")
+      action(obj);
+    else for (var field in obj) {
+      if (["geometries", "coordinates", "features", "geometry"].indexOf(field)!=-1)
+        inspect(obj[field]);
+      if (field.search(/^[0-9]+$/)>=0)
+        inspect(obj[field]);
+    }
+  }
+  apply_action(GeoJSON);
+};
+
+// Calculate a bounding box.
 
 exports.bbox=function(GeoJSON) {
   var bbox=[Infinity,Infinity,-Infinity,-Infinity];
-  ;function update_bbox(obj) {
-    if (typeof(obj)!="object") return;
-    if (typeof(obj[0])=="number") {
-      bbox[0]=(bbox[0]<obj[0])?bbox[0]:obj[0];
-      bbox[1]=(bbox[1]<obj[1])?bbox[1]:obj[1];
-      bbox[2]=(bbox[2]>obj[0])?bbox[2]:obj[0];
-      bbox[3]=(bbox[3]>obj[1])?bbox[3]:obj[1];
-    } else for (var field in obj) {
-      if (["geometries", "coordinates", "features", "geometry"].indexOf(field)!=-1)
-        update_bbox(obj[field]);
-      if (field.search(/^[0-9]+$/)>=0)
-        update_bbox(obj[field]);
-    }
-  }
-  update_bbox(GeoJSON);
+  exports.eachCoord(GeoJSON, function(coord) {
+    bbox[0]=(bbox[0]<coord[0])?bbox[0]:coord[0];
+    bbox[1]=(bbox[1]<coord[1])?bbox[1]:coord[1];
+    bbox[2]=(bbox[2]>coord[0])?bbox[2]:coord[0];
+    bbox[3]=(bbox[3]>coord[1])?bbox[3]:coord[1];
+  });
   return bbox;
-}
+};
 
 // Also define some kind of 'importance' here to choose
 // what items to draw first. We use the bbox circumfence
@@ -26,4 +34,4 @@ exports.bbox=function(GeoJSON) {
 
 exports.size=function(GeoJSON) {
   return (GeoJSON.bbox[2]-GeoJSON.bbox[0])+(GeoJSON.bbox[3]-GeoJSON.bbox[1]);
-}
+};
