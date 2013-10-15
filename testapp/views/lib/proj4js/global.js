4942c327cc68cab71584a572f7b2cdefa5951796
Oscar Reinecke <reinecke@posteo.org>
2013-10-15 17:17:27 +0200

    Removed some apparently orphaned libs and added two CRSs.

--- testapp/views/lib/proj4js/global.js
+++ testapp/views/lib/proj4js/global.js
@@ -4,6 +4,9 @@ module.exports = function(defs) {
   defs('EPSG:4269', "+title=NAD83 (long/lat) +proj=longlat +a=6378137.0 +b=6356752.31414036 +ellps=GRS80 +datum=NAD83 +units=degrees");
   defs('EPSG:3857', "+title=WGS 84 / Pseudo-Mercator +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs");
 
+  defs('EPSG:3396', "+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +units=m +no_defs");
+  defs('EPSG:3397', "+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4500000 +y_0=0 +ellps=bessel +units=m +no_defs");
+
   defs['EPSG:3785'] = defs['EPSG:3857']; // maintain backward compat, official code is 3857
   defs.GOOGLE = defs['EPSG:3857'];
   defs['EPSG:900913'] = defs['EPSG:3857'];
