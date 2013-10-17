a688f6ef9819ea96013c62af38acda8070795222
Oscar Reinecke <reinecke@posteo.org>
2013-10-17 14:44:43 +0200

    Collected 'doc' and 'GeoJSON' object into one emit().
    Because _list/bbox-filter does not care if those are one or two rows. If we
    work with 2000k+ Point-GeoJSON (that are likely to be assigned to just one
    document), we would need to process 40000k rows otherwise which is stupid.

--- testapp/views/geojson/map.js
+++ testapp/views/geojson/map.js
@@ -5,6 +5,7 @@ function(doc) {
   var id=doc["GeoJSON" in doc?"_id":"GeoJSON_clone"];
   // abort if no GeoJSON is set at all
   if (id==null) return;
+  var val={};
   if (id===doc._id) {
     // I need a copy that can be modified
     var GeoJSON=utils.clone(doc.GeoJSON);
@@ -12,14 +13,14 @@ function(doc) {
     GeoJSON.bbox=utils.bbox(GeoJSON);
     // provide an error for reduced polygons
     GeoJSON.error=0;
-    emit(id, {GeoJSON: [GeoJSON], size: utils.size(GeoJSON)});
+    val={GeoJSON: [GeoJSON], size: utils.size(GeoJSON)};
   };
-  var properties={_id:doc._id, type:doc.type};
+  val.doc={_id:doc._id, type:doc.type};
   for (var field in doc) {
     // fields with lowecase letters are english and kind of 'internal'
     if (field.search(/^[A-Z]/)==-1) continue;
     if (field.search("GeoJSON")!=-1) continue;
-    properties[field]=doc[field];
+    val.doc[field]=doc[field];
   }
-  emit(id, {doc:properties});
+  emit(id, val);
 }
