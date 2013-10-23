efa0625435b8a8b782c6d5f8781f606b354b697b
Oscar Reinecke <reinecke@posteo.org>
2013-10-23 14:36:16 +0200

    Revert "Modified list to submit all options instead of just keys."
    This reverts commit 81ad0b8d241231fec33364ca2c449467a62d782c.

--- testapp/_attachments/script/jquery.couch.js
+++ testapp/_attachments/script/jquery.couch.js
@@ -817,13 +817,15 @@
           var data = null;
           if (options['keys']) {
             type = 'POST';
-            data = options;
+            var keys = options['keys'];
+            delete options['keys'];
+            data = toJSON({'keys': keys });
           }
           ajax({
               type: type,
               data: data,
               url: this.uri + '_design/' + list[0] +
-                   '/_list/' + list[1] + '/' + view + (type=="GET"?encodeOptions(options):"");
+                   '/_list/' + list[1] + '/' + view + encodeOptions(options)
               },
               ajaxOptions, 'An error occured accessing the list'
           );
