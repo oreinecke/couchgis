ffe6e35805e2d563c4ee9aac0b4937c658c0c975
Oscar Reinecke <reinecke@posteo.org>
2013-10-29 16:41:21 +0100

    Placed geometry error in _view/bbox-size.
    This way I only need to query _view/geojson if geometry level has changed and
    this info is already provided by _view/bbox-size. I worked around an additional
    id list for update_geometry() by setting item.keep to a boolean.
    Unfortunately bbox-size has to descend the entire reduced geometry tree without
    emitting any geometry at all. This seems kind of useless but I don't see any
    other way to reproduce the errors emitted in _view/geojson.
    I certainly will not use the client to store errors in any way because this
    implies ghastly code ahead!!!

--- testapp/views/bbox-size/map.js
+++ testapp/views/bbox-size/map.js
@@ -9,7 +9,14 @@ function(doc) {
     utils.toWGS84(GeoJSON);
     utils.bbox(GeoJSON);
     utils.size(GeoJSON);
-    emit(id, {GeoJSON:{bbox:GeoJSON.bbox, size:GeoJSON.size}});
+    // Provide list of reduced geometry errors as in _view/geojson.
+    for (var error=Infinity;error>0;error=error*0.5) {
+      error*=(error>=5e-6);
+      var simplified_GeoJSON=utils.simplify(utils.clone(GeoJSON),error);
+      if (error<Infinity || simplified_GeoJSON.error==0)
+        emit(id, {GeoJSON:{bbox:GeoJSON.bbox, size:GeoJSON.size, error:simplified_GeoJSON.error}});
+      error=simplified_GeoJSON.error;
+    }
   };
   emit(id, {doc:{_id:doc._id, type:doc.type}});
 }
