30205b353dad6f86c300a7ece54b46867645f225
Oscar Reinecke <reinecke@posteo.org>
2013-11-14 15:27:46 +0100

    Revised _list/bbox-filter to use function variables.
    This is probably what they call 'enhanced algorithms' or something:
    For instance for the bounding box check, I don't need to check if GeoJSON
    contains bbox for each row; If it's in the first row, it's gonna be in all of
    them.
    Since function calls are expensive, I still could end up worse than before, but
    it sure looks 'enhanced'!!!!

--- testapp/lists/bbox-filter.js
+++ testapp/lists/bbox-filter.js
@@ -5,12 +5,15 @@ function(head, req) {
   start({'headers':{'Content-Type':'application/json;charset=utf-8'}});
   var body=(req.body=="undefined"?{}:JSON.parse(req.body));
   var bbox=body.bbox;
-  // initialize to infinite bbox if none provided
-  if (bbox==null || typeof(bbox)!="object")
-    bbox=[-Infinity,-Infinity,Infinity,Infinity];
+  // initialize to false if none provided
+  if (bbox==null || typeof(bbox)!="object") bbox=false;
   // we need to limit output and server load
   var limit=body.limit;
   if (typeof(limit)!="number") limit=Infinity;
+  if (!limit) {
+    send('{}\n');
+    return;
+  }
   var error=body.error;
   // allow reduced polygons to deviate up to this amount
   if (typeof(error)!="number") error=0.0;
@@ -18,44 +21,65 @@ function(head, req) {
   var types=body.types;
   if (typeof(types)!="object" || typeof(types.indexOf)!="function")
     types=null;
-  var row={}, last_key, last_GeoJSON, docs=[];
+  var row={}, GeoJSON, last_GeoJSON, last_key, docs=[];
   var items={};
-  if (!limit) {
-    send(JSON.stringify(items));
-    return;
-  }
+  // Use function variables to work around useless repetition:
+  // i) Check if we have to read more items.
+  var proceed=function() {
+    if (limit!=Infinity) proceed=function() {
+      return (--limit);
+    }; else proceed=function() { return true; };
+    return proceed();
+  };
+  // i) Check if document type matches selection.
+  var type_matches=function() {
+    if (types) type_matches=function() {
+      return (types.indexOf(row.value.doc.type!==-1));
+    }; else type_matches=function() { return true; };
+    return type_matches();
+  };
+  // ii) Check if geometry bbox is outside bbox.
+  var outside_bbox=function() {
+    if ('bbox' in GeoJSON && bbox) outside_bbox=function() {
+      return (bbox[0]>GeoJSON.bbox[2]||GeoJSON.bbox[0]>bbox[2]|| 
+              bbox[1]>GeoJSON.bbox[3]||GeoJSON.bbox[1]>bbox[3]);
+    }; else outside_bbox=function() { return false; };
+    return outside_bbox();
+  };
+  // iii) Send comma and newline as we reach the 2nd item.
+  var send_separator=function() {
+    send_separator=function() {send(',\n');};
+  };
+  send('{');
   while (row) {
     if (row) row=getRow();
     if (last_key && (row==null || last_key!=row.key)) {
       if (last_GeoJSON && docs.length) {
         items[last_key]={GeoJSON:last_GeoJSON,docs:docs};
-        limit--;
-        // don't look further if list is complete
-        if (!limit) break;
+        send_separator();
+        send('"'+last_key+'":{"GeoJSON":'+JSON.stringify(last_GeoJSON)
+                             +',"docs":'+JSON.stringify(docs)+'}');
+        if (!proceed()) break;
       }
       docs=[];
       last_GeoJSON=null;
     }
     if (!row) continue;
     last_key=row.key;
-    if ('doc' in row.value && (!types||types.indexOf(row.value.doc.type)!==-1))
-      docs.push(row.value.doc);
-    var GeoJSON=row.value.GeoJSON;
+    var doc=row.value.doc;
+    if (row.value.doc && type_matches()) docs.push(row.value.doc);
+    GeoJSON=row.value.GeoJSON;
     // evaluation of geomeric properties follows:
     if (GeoJSON==null) continue;
     // skip if outside bbox
-    if ('bbox' in GeoJSON && (bbox[0]>GeoJSON.bbox[2]||GeoJSON.bbox[0]>bbox[2]|| 
-                              bbox[1]>GeoJSON.bbox[3]||GeoJSON.bbox[1]>bbox[3]))
-      continue;
-    if ('error' in GeoJSON) {
-      // skip if geometry has too few details
-      if (GeoJSON.error>error) continue;
-      // skip if we already have a more detailed version
-      if (last_GeoJSON && last_GeoJSON.error>=GeoJSON.error) continue;
-    }
+    if (outside_bbox()) continue;
+    // skip if geometry has too few details
+    if (GeoJSON.error>error) continue;
+    // skip if we already have a more detailed version
+    if (last_GeoJSON && last_GeoJSON.error>=GeoJSON.error) continue;
     last_GeoJSON=GeoJSON;
   }
-  send(JSON.stringify(items));
+  send('}\n');
   // Uncomment this next line as soon as any(more) trouble arises: for some
   // reason, the list crashes if rows are left after the function returned.
   // This happens only if keys are specified in the request body.
