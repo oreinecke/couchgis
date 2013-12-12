41c99c184a37e7b16bb4f7b7681f6cf70bd72c8b
Oscar Reinecke <reinecke@posteo.org>
2013-12-12 15:02:04 +0100

    Added lib/range.
    Because I need a small utility to test if a given date falls inside a time
    range or if two time ranges overlap and so on. It should handle dates as
    strings such as "05.5.2012" or just "1992" or "5.1999-2001" etc, so if the day
    or month is omitted it should assume that the whole day/month was meant. Years
    still have to be four digits and it does not care about leap days or how many
    days a month has.
    I found this waaay harder to write than I thought. Testing was a nightmare!!!

---
+++ testapp/views/lib/range.js
@@ -0,0 +1,97 @@
+// Convert date to [YYYY,MM,DD] without any leading zeros.
+
+function toDate(date) {
+  if (date=="") return [];
+  date=date.split('.').reverse();
+  for (var d=0;d<date.length;d++)
+    date[d]=parseInt(date[d].replace(/^0/g, ""));
+  return date;
+}
+
+// Convert range or date string to range object.
+
+function toRange(range) {
+  if (!range) return;
+  range=String(range).split('-');
+  return {
+    begin:toDate(range[0]),
+    end:toDate(range[range.length-1])
+  };
+}
+exports.toRange=toRange;
+
+// Convert range to string.
+
+exports.toString=function(range) {
+  var begin=range.begin.reverse().join(',');
+  var end=range.end.reverse().join(',');
+  if (begin==end) return begin;
+  else return begin+'-'+end;
+};
+
+// Returns true if date a<=b.
+
+function less_or_equal(a, b) {
+  for (var d=0;d<3;d++) {
+    if (d==a.length) return true;
+    if (d==b.length) return false;
+    if (a[d]<b[d]) return true;
+    if (a[d]>b[d]) return false;
+  }
+  return true;
+}
+
+// Returns true if date a>=b, not equal to calling less_or_equal(b, a).
+
+function greater_or_equal(a, b) {
+  for (var d=0;d<3;d++) {
+    if (d==a.length) return true;
+    if (d==b.length) return false;
+    if (a[d]>b[d]) return true;
+    if (a[d]<b[d]) return false;
+  }
+  return true;
+}
+
+// Returns true if range a contains range b.
+
+exports.contains=function(a, b) {
+  if (typeof(a)=="string") a=toRange(a);
+  var a1=a.begin, a2=a.end;
+  if (typeof(b)=="string") b=toRange(b);
+  var b1=b.begin, b2=b.end;
+  return less_or_equal(a1,b1) && greater_or_equal(a2,b2);
+};
+
+// Returns true if range a overlaps with range b.
+
+exports.intersects=function(a, b) {
+  if (typeof(a)=="string") a=toRange(a);
+  var a1=a.begin, a2=a.end;
+  if (typeof(b)=="string") b=toRange(b);
+  var b1=b.begin, b2=b.end;
+  for (var d=0;d<3;d++) {
+    if (d==a1.length || d==b2.length) break;
+    if (a1[d]>b2[d]) return false;
+    if (a1[d]<b2[d]) break;
+  }
+  for (var d=0;d<3;d++) {
+    if (d==b1.length || d==a2.length) break;
+    if (b1[d]>a2[d]) return false;
+    if (b1[d]<a2[d]) break;
+  }
+  return true;
+};
+
+// Return minimum range that includes a and b.
+
+exports.expand=function(a, b) {
+  if (typeof(a)=="string") a=toRange(a);
+  var a1=a.begin, a2=a.end;
+  if (typeof(b)=="string") b=toRange(b);
+  var b1=b.begin, b2=b.end;
+  return {
+    begin:(less_or_equal(a1,b1)?a1:b1),
+    end:(greater_or_equal(a2,b2)?a2:b2)
+  };
+};
