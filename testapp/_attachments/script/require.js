69a8134eda744a41b089943cb9be0382f1ec1350
Oscar Reinecke <reinecke@posteo.org>
2013-12-13 16:43:50 +0100

    Revised script/require.js.
    It now loads dependencies recursively and accepts relative paths. Critical
    variables have been shadowed before lib compilation, so this seems pretty safe
    now. I did not shadow 'window', 'document', '$' etc. because none of my
    libraries are THIS ill-willed.
    JavaScript never ceases to surprise me with its awesome closure and scope
    power. The script still looks pretty sweet despite its complex task.

--- testapp/_attachments/script/require.js
+++ testapp/_attachments/script/require.js
@@ -1,6 +1,7 @@
-// Loads CommonJS libraries nested in CouchDB design docs with an identical
-// syntax e.g. var utils=require("views/lib/utils");
-// No dependencies are checked and the use of eval is highly critical.
+// Loads CommonJS libraries nested in CouchDB design docs using an identical
+// syntax. Requires inside libraries are evaluated and relative paths are
+// supported. Critical variables have been shadowed but the library would still
+// able to manipulate other globals i.e. window, document, $ etc.
 
 var require;
 
@@ -9,21 +10,32 @@ $(document).ready(function() {
     var ddoc;
     // get design document and save an abreviated copy
     $.getJSON("./", function(data) {
-      ddoc={views:data.views};
+      ddoc={views:{lib:data.views.lib}};
     });
-    return function(lib) {
-      // hide temp variables in here
-      (function() {
-        var parts=lib.split('/');
-        lib=ddoc;
-        for (var p=0;p<parts.length;p++) {
-          console.log(parts[p]);
-          lib=lib[parts[p]];
-        }
-      }());
+    var result=function(lib, root) {
+      var parts=lib.split('/');
+      if (parts[0]==".") lib=root;
+      else lib=ddoc;
+      for (var p=0;p<parts.length;p++) {
+        // save location for nested requires
+        if (p==parts.length-1) root=lib;
+        lib=lib[parts[p]];
+      }
+      // provide exports
       var exports={};
-      eval(lib);
+      // hide require and provide last location
+      var require=function(lib) {
+        result(lib, root);
+      };
+      // hide vars from lib compilation
+      (function() {
+        var ddoc, root parts, result;
+        eval(lib);
+      }());
       return exports;
+    };
+    return function(lib) {
+      result(lib, ddoc);
     };
   }();
 });
