617b2133948679235a431d62935b79e0773ae810
Oscar Reinecke <reinecke@posteo.org>
2013-12-16 13:01:15 +0100

    Fixed a couple of stupid coding errors.

--- testapp/_attachments/script/require.js
+++ testapp/_attachments/script/require.js
@@ -1,7 +1,7 @@
 // Loads CommonJS libraries nested in CouchDB design docs using an identical
 // syntax. Requires inside libraries are evaluated and relative paths are
 // supported. Critical variables have been shadowed but the library would still
-// able to manipulate other globals i.e. window, document, $ etc.
+// be able to manipulate other globals i.e. window, document, $ etc.
 
 var require;
 
@@ -29,13 +29,13 @@ $(document).ready(function() {
       };
       // hide vars from lib compilation
       (function() {
-        var ddoc, root parts, result;
+        var ddoc, root, parts, result;
         eval(lib);
       }());
       return exports;
     };
     return function(lib) {
-      result(lib, ddoc);
+      return result(lib, ddoc);
     };
   }();
 });
