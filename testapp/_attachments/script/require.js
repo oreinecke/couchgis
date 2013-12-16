475e593c6f1a5d5ac996ef8a8327fbab5ed25e01
Oscar Reinecke <reinecke@posteo.org>
2013-12-16 15:57:06 +0100

    Removed $(document).ready from script/require.js.
    Because the design doc should be loaded before any libs are required.

--- testapp/_attachments/script/require.js
+++ testapp/_attachments/script/require.js
@@ -3,39 +3,35 @@
 // supported. Critical variables have been shadowed but the library would still
 // be able to manipulate other globals i.e. window, document, $ etc.
 
-var require;
-
-$(document).ready(function() {
-  require=function() {
-    var ddoc;
-    // get design document and save an abreviated copy
-    $.getJSON("./", function(data) {
-      ddoc={views:{lib:data.views.lib}};
-    });
-    var result=function(lib, root) {
-      var parts=lib.split('/');
-      if (parts[0]==".") lib=root;
-      else lib=ddoc;
-      for (var p=0;p<parts.length;p++) {
-        // save location for nested requires
-        if (p==parts.length-1) root=lib;
-        lib=lib[parts[p]];
-      }
-      // provide exports
-      var exports={};
-      // hide require and provide last location
-      var require=function(lib) {
-        result(lib, root);
-      };
-      // hide vars from lib compilation
-      (function() {
-        var ddoc, root, parts, result;
-        eval(lib);
-      }());
-      return exports;
+var require=function() {
+  var ddoc;
+  // get design document and save an abreviated copy
+  $.getJSON("./", function(data) {
+    ddoc={views:{lib:data.views.lib}};
+  });
+  var result=function(lib, root) {
+    var parts=lib.split('/');
+    if (parts[0]==".") lib=root;
+    else lib=ddoc;
+    for (var p=0;p<parts.length;p++) {
+      // save location for nested requires
+      if (p==parts.length-1) root=lib;
+      lib=lib[parts[p]];
+    }
+    // provide exports
+    var exports={};
+    // hide require and provide last location
+    var require=function(lib) {
+      result(lib, root);
     };
-    return function(lib) {
-      return result(lib, ddoc);
-    };
-  }();
-});
+    // hide vars from lib compilation
+    (function() {
+      var ddoc, root, parts, result;
+      eval(lib);
+    }());
+    return exports;
+  };
+  return function(lib) {
+    return result(lib, ddoc);
+  };
+}();
