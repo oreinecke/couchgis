9db10dbb2569afc2926e59803bdda5de778087ec
Oscar Reinecke <reinecke@posteo.org>
2014-01-09 14:19:12 +0100

    Added unified time range to _view/dimensions reduce output.
    CommonJS is unavailable for reduce functions so I had to quote it from
    lib/range. Avoided the use of range/toRange or range/toString because they are
    most likely to be changed for more flexibility and I would end up with more
    code maintenance. The expand function could look simpler for sure.

--- testapp/views/dimensions/map.js
+++ testapp/views/dimensions/map.js
@@ -2,10 +2,11 @@
 
 function(doc) {
   var utils=require('views/lib/utils');
+  var range=require('views/lib/range');
   if (doc.GeoJSON==null) return;
   var GeoJSON=utils.clone(doc.GeoJSON);
   utils.toWGS84(GeoJSON);
   utils.bbox(GeoJSON);
   utils.size(GeoJSON);
-  emit(GeoJSON.size, {bbox:GeoJSON.bbox, time:doc.time});
+  emit(GeoJSON.size, {bbox:GeoJSON.bbox, range:range.toRange(doc.time)});
 }
--- testapp/views/dimensions/reduce.js
+++ testapp/views/dimensions/reduce.js
@@ -1,13 +1,47 @@
-// Unify bboxes for viewport initialization.
+// Unifies bboxes for viewport initialization.
+// Also unifies time ranges as general info.
 
 function(keys, values, rereduce) {
+  ;function less_or_equal(a, b) {
+    for (var d=0;d<3;d++) {
+      if (d==a.length) return true;
+      if (d==b.length) return false;
+      if (a[d]<b[d]) return true;
+      if (a[d]>b[d]) return false;
+    }
+    return true;
+  }
+  ;function greater_or_equal(a, b) {
+    for (var d=0;d<3;d++) {
+      if (d==a.length) return true;
+      if (d==b.length) return false;
+      if (a[d]>b[d]) return true;
+      if (a[d]<b[d]) return false;
+    }
+    return true;
+  }
+  ;function expand(a,b) {
+    var a1=a.begin, a2=a.end;
+    var b1=b.begin, b2=b.end;
+    var result={
+      begin:(less_or_equal(a1,b1)?a1:b1),
+      end:(greater_or_equal(a2,b2)?a2:b2)
+    };
+    if (!a1.length) result.begin=b1;
+    if (!b1.length) result.begin=a1;
+    if (!a2.length) result.end=b2;
+    if (!b2.length) result.end=a2;
+    return result;
+  }
   var bbox=values[0].bbox;
+  var range=values[0].range;
   for (var v=1;v<values.length;v++) {
     var bbox2=values[v].bbox;
     bbox[0]=(bbox[0]<bbox2[0])?bbox[0]:bbox2[0];
     bbox[1]=(bbox[1]<bbox2[1])?bbox[1]:bbox2[1];
     bbox[2]=(bbox[2]>bbox2[2])?bbox[2]:bbox2[2];
     bbox[3]=(bbox[3]>bbox2[3])?bbox[3]:bbox2[3];
+    range=expand(range, values[v].range);
   }
-  return {bbox:bbox};
+  return {bbox:bbox, range:range};
 }
