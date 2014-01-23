9856f092bf817dfda2552d0661b5b4c1c538a448
Oscar Reinecke <reinecke@posteo.org>
2014-01-23 17:42:16 +0100

    Fixed misplaced bracket in _view/fields-values

--- testapp/views/fields-values/map.js
+++ testapp/views/fields-values/map.js
@@ -7,7 +7,7 @@ function(doc) {
     var val=doc[field];
     if (field[0].search(/[A-Z]/)!=0) continue;
     if (field.search(/^GeoJSON/)==0) continue;
-    if (typeof(val!="string")) val=typeof(val);
+    if (typeof(val)!=="string") val=typeof(val);
     emit([type, field, val]);
   }
 }
