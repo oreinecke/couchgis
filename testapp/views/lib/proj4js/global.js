738c401e3be3d14631367afa1d9793aaa6efe4d2
Oscar Reinecke <reinecke@posteo.org>
2014-01-27 13:06:22 +0100

    Added EPSG:31469 to lib/proj4js.

--- testapp/views/lib/proj4js/global.js
+++ testapp/views/lib/proj4js/global.js
@@ -9,6 +9,7 @@ module.exports = function(defs) {
 
   defs('EPSG:31467', "+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=4499998.5 +y_0=65 +ellps=bessel +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs");
   defs('EPSG:31468', "+proj=tmerc +lat_0=0 +lon_0=12 +k=1 +x_0=4499998.5 +y_0=65 +ellps=bessel +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs");
+  defs('EPSG:31469', "+proj=tmerc +lat_0=0 +lon_0=15 +k=1 +x_0=5499998.5 +y_0=65 +ellps=bessel +towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7 +units=m +no_defs");
 
   defs['EPSG:3785'] = defs['EPSG:3857']; // maintain backward compat, official code is 3857
   defs.GOOGLE = defs['EPSG:3857'];
