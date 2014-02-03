9a4ee9340bfc3df7149ed64a5637aa793b3d5e86
Oscar Reinecke <reinecke@posteo.org>
2014-02-03 16:24:48 +0100

    Fixed wrong autocomplete suggestions.
    That certainly was easy: I just put value.label at the highest priority and now
    suggestions keep showing up even after an item was selected (and its value was
    copied into the text input).
    I definitely should file some pull requests to these people, this seems a lot
    like work in progress.

--- testapp/_attachments/script/jquery-ui-1.10.4.custom.js
+++ testapp/_attachments/script/jquery-ui-1.10.4.custom.js
@@ -1872,7 +1872,7 @@ $.extend( $.ui.autocomplete, {
 	filter: function(array, term) {
 		var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), "i" );
 		return $.grep( array, function(value) {
-			return matcher.test( value.label || value.value || value );
+			return matcher.test( value.value || value.label || value );
 		});
 	}
 });
