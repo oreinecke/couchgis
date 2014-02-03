ecb2b0e8c3007121dadf13c77c61e94753e7d756
Oscar Reinecke <reinecke@posteo.org>
2014-02-03 15:59:37 +0100

    Improved _view/fields-values.
    This new view makes more use of the reduce function by providing a count, a
    range for numbers and if the field happens to be an object or array, I can
    probably think of something too.
    'doc.type' was moved to the end of the array key because I want to histogram
    identical field names of different doc types.

--- testapp/views/fields-values/map.js
+++ testapp/views/fields-values/map.js
@@ -1,13 +1,18 @@
 // Sample values of fields for auto-completion.
 
 function(doc) {
-  var type=doc.type;
-  if (type==null) return;
+  if (doc.type==null) return;
   for (var field in doc) {
     var val=doc[field];
     if (field[0].search(/[A-Z]/)!=0) continue;
     if (field.search(/^GeoJSON/)==0) continue;
-    if (typeof(val)!=="string") val=typeof(val);
-    emit([type, field, val]);
+    // store detailed field info in summary
+    var summary={count:1,type:typeof(val)};
+    if (typeof(val)==="number") {
+      summary.min=val;
+      summary.max=val;
+      val=null;
+    }
+    emit([field, val, doc.type], summary);
   }
 }
--- testapp/views/fields-values/reduce.js
+++ testapp/views/fields-values/reduce.js
@@ -1,1 +1,12 @@
-_count
+function reduce(keys, values, rereduce) {
+  var result=values[0];
+  for (var v=1;v<values.length;v++) {
+    var value=values[v];
+    result.count+=value.count;
+    if (result.type==="number") {
+      if (value.min<result.min) result.min=value.min;
+      if (value.max>result.max) result.max=value.max;
+    }
+  }
+  return result;
+}
