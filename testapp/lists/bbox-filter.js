f68faf18c3ba0b0d3014ce66b710f63e037532b3
Oscar Reinecke <reinecke@posteo.org>
2014-02-17 16:18:01 +0100

    Improved _list/bbox-filter.
    This commit is a mess but here's what it does:
    - Moved some var declarations inside the while-statement. This makes no real
      difference but it increases readability.
    - Changed semantics from outside_bbox to inside_bbox because that allows me to
      initialize every function variable as the atomic pass() statement.
    - inside_bbox then accepts the other bbox as argument 'bbox2'.
    - The commented data-exhaustion statement was kind-of wrong (although never in
      use).
    - I worked out a very crude comparison of similar geometries: it just checks if
      bounding boxes are close to one another. It still may suffice to merge almost
      duplicate geometries being attached to their documents.
      This is an inherent problem of ESRI shapefiles!! It was a poor design in 1998
      and it is poor today!!!

--- testapp/lists/bbox-filter.js
+++ testapp/lists/bbox-filter.js
@@ -13,23 +13,34 @@ function(head, req) {
   if (!limit) return '{}\n';
   // allow reduced polygons to deviate up to this amount
   var error=0.0;
-  if ('error' in options) error=options.error;
-  var row=getRow(), GeoJSON, last_GeoJSON, last_key;
+  function pass() {return true;}
   // Use function variables to work around useless repetition:
   // i) Check if we have to read more items.
-  var proceed=function() {
-    if (limit!=Infinity) proceed=function() {
-      return (--limit);
-    }; else proceed=function() { return true; };
-    return proceed();
+  var proceed=pass;
+  if (limit!=Infinity) proceed=function() {
+    return (--limit);
   };
-  // iii) Check if geometry bbox is outside bbox.
-  var outside_bbox=function() {
-    if ('bbox' in GeoJSON && bbox) outside_bbox=function() {
-      return (bbox[0]>GeoJSON.bbox[2]||GeoJSON.bbox[0]>bbox[2]||
-              bbox[1]>GeoJSON.bbox[3]||GeoJSON.bbox[1]>bbox[3]);
-    }; else outside_bbox=function() { return false; };
-    return outside_bbox();
+  // ii) This is a crude optionial check for similar geometry.
+  var similarity=Infinity;
+  if ('similarity' in options) similarity=options.similarity;
+  var bbox_is_similar=pass;
+  if (similarity!=Infinity) {
+    similarity*=similarity;
+    bbox_is_similar=function(bbox2) {
+      var d0=bbox[0]-bbox2[0], d1=bbox[1]-bbox2[1],
+          d2=bbox[2]-bbox2[2], d3=bbox[3]-bbox2[3];
+      return d0*d0+d1*d1<=similarity && d2*d2+d3*d3<=similarity;
+    };
+  }
+  if ('error' in options) error=options.error;
+  var row=getRow(), GeoJSON;
+  // iii) Check if bboxes intersect.
+  var inside_bbox=function() {
+    if ('bbox' in GeoJSON && bbox) inside_bbox=function(bbox2) {
+      return (bbox[0]<=bbox2[2]&&bbox2[0]<=bbox[2]&&
+              bbox[1]<=bbox2[3]&&bbox2[1]<=bbox[3]);
+    }; else inside_bbox=pass;
+    return inside_bbox(GeoJSON.bbox);
   };
   // iv) Send comma and newline as we reach the 2nd item.
   var send_separator=function() {
@@ -38,15 +49,10 @@ function(head, req) {
   send('{');
   while (row) {
     GeoJSON=row.value.GeoJSON;
-    if (outside_bbox()) {
-      // skip to next key if outside bbox
-      last_key=row.key;
-      do row=getRow();
-      while (row && row.key==last_key);
-    } else {
+    if (inside_bbox(GeoJSON.bbox) && bbox_is_similar(GeoJSON.bbox)) {
       // scan rows for matching detail level
-      last_GeoJSON=row.value.GeoJSON;
-      last_key=row.key;
+      var last_GeoJSON=row.value.GeoJSON;
+      var last_key=row.key;
       row=getRow();
       while (row && row.key==last_key) {
         GeoJSON=row.value.GeoJSON;
@@ -57,12 +63,17 @@ function(head, req) {
       send_separator();
       send('"'+last_key+'":{"GeoJSON":'+JSON.stringify(last_GeoJSON)+'}');
       if (!proceed()) break;
+    } else {
+      // skip to next key if outside bbox
+      var last_key=row.key;
+      do row=getRow();
+      while (row && row.key==last_key);
     }
   }
   return '}\n';
   // Uncomment this next line as soon as any(more) trouble arises: for some
   // reason, the list crashes if rows are left after the function returned.
   // This happens only if keys are specified in the request body.
-  // if (req.body.indexOf(/"keys"/)!=-1) while (getRow());
+  // if (/"keys"/.test(req.body)) while (getRow());
 }
 
