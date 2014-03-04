d1c052865f8107fa9a915a8950c2ff86d4675481
Oscar Reinecke <reinecke@posteo.org>
2014-03-04 16:08:39 +0100

    Added xml table to _list/export's file types.
    I employed some very crafty single-instanciated-lookup-objects to initialize
    the response header without making too much fuzz. If no fields are given, the
    list yields all there is, with optional geometry id and revision as the first
    columns. A column will be rendered for each specified field, even if we ended
    up with an empty column.

--- testapp/lists/export.js
+++ testapp/lists/export.js
@@ -2,6 +2,9 @@
 
 function(head, req) {
   var filename=req.query.filename || "export";
+  var filetype=({
+    xml:"xml", geojson:"geojson"
+  })[req.query.filetype] || "xml";
   var download='download' in req.query;
   var indexes=req.query.compressed_keys;
   if (indexes)
@@ -12,8 +15,13 @@ function(head, req) {
   var include_geojson_id='include_geojson_id' in req.query;
   var index=0;
   start({'headers':{
-    'Content-Type':'application/json;charset=utf-8',
-    'Content-Disposition':download?'attachment;filename="'+filename+'.geojson"':undefined
+    'Content-Type':{
+      geojson:'application/json;charset=utf-8',
+      xml:'text/xml'
+    }[filetype],
+    'Content-Disposition':{
+      true:'attachment;filename="'+filename+'.'+filetype
+    }[download]
   }});
   var features=[];
   var row=getRow();
@@ -32,6 +40,8 @@ function(head, req) {
           delete doc._rev;
           delete doc._id;
         }
+        if (include_geojson_id)
+          doc.GeoJSON_clone=row.key[0];
         for (var field in doc) {
           if (fields==null) break;
           if (field[0].search(/[A-ZÄÖÜ]/)!=0) continue;
@@ -39,8 +49,6 @@ function(head, req) {
           if (fields.search('(^|:)'+field+'(:|$)')!==-1) continue;
           delete doc[field];
         }
-        if (include_geojson_id)
-          doc.GeoJSON_clone=row.key[0];
         features.push({
           type:"Feature",
           geometry:geometry,
@@ -52,8 +60,66 @@ function(head, req) {
       index++;
     }
   }
-  return JSON.stringify({
-    crs:{type:"name", properties:{name:"urn:ogc:def:crs:OGC:1.3:CRS84"}},
-    name:filename, type:"FeatureCollection", features:features
-  });
+  switch(filetype) {
+  case "geojson":
+    return JSON.stringify({
+      crs:{type:"name", properties:{name:"urn:ogc:def:crs:OGC:1.3:CRS84"}},
+      name:filename,
+      features:features,
+      type:"FeatureCollection"
+    });
+  case "xml":
+    if (fields==null) {
+      fields=[];
+      if (include_geojson_id) fields.unshift("GeoJSON_clone");
+      if (include_revision) fields=["_id", "_rev"].concat(fields);
+      for (var f=0;f<features.length;f++)
+      for (var prop in features[f].properties)
+        if (fields.indexOf(prop)===-1) fields.push(prop);
+      // push GeoJSON_clone to the left
+    } else {
+      fields=["type", "time"].concat(fields.split(':'));
+      if (include_geojson_id) fields.unshift("GeoJSON_clone");
+      if (include_revision) fields=["_id", "_rev"].concat(fields);
+    }
+    send('<?xml version="1.0"?>');
+    send('<?mso-application progid="Excel.Sheet"?>');
+    send('<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"');
+    send(' xmlns:o="urn:schemas-microsoft-com:office:office"');
+    send(' xmlns:x="urn:schemas-microsoft-com:office:excel"');
+    send(' xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"');
+    send(' xmlns:html="http://www.w3.org/TR/REC-html40">');
+    send('<DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">');
+    send(' <Version>14.00</Version>');
+    send('</DocumentProperties>');
+    send('<OfficeDocumentSettings xmlns="urn:schemas-microsoft-com:office:office">');
+    send('</OfficeDocumentSettings>');
+    send('<ExcelWorkbook xmlns="urn:schemas-microsoft-com:office:excel">');
+    send('</ExcelWorkbook>');
+    send('<Worksheet ss:Name="'+filename+'">');
+    send('<Table>');
+    send('<Row>');
+    for (var g=0;g<fields.length;g++)
+      send('<Cell><Data ss:Type="String">'+fields[g]+'</Data></Cell>');
+    send('</Row>');
+    for (var f=0;f<features.length;f++) {
+      send('<Row>');
+      var properties=features[f].properties;
+      for (var g=0;g<fields.length;g++) {
+        var field=fields[g];
+        var data=properties[field];
+        if (typeof data==="number")
+          send('<Cell><Data ss:Type="Number">'+data+'</Data></Cell>');
+        else if (typeof data==="string")
+          send('<Cell><Data ss:Type="String">'+data+'</Data></Cell>');
+        else if (typeof data==="boolean")
+          send('<Cell><Data ss:Type="Boolean">'+data?1:0+'</Data></Cell>');
+        else send('<Cell/>');
+      }
+      send('</Row>');
+    }
+    send('</Table>');
+    send('</Worksheet>');
+    send('</Workbook>');
+  }
 }
