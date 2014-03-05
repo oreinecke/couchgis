24b1f61b519e139f12d8535b676f50b6118de065
Oscar Reinecke <reinecke@posteo.org>
2014-03-05 14:13:05 +0100

    Reduced amount of rows processed by _list/export.
    If key compression is used for large datasets, _list/export might end up
    skipping a lot of keys at the beginning. Unfortunately we don't have a
    skipRow() function to bypass JSON.parse of exhaustive geometry objects, wink,
    wink.
    I worked around the offset by providing start_key and end_key. And because I
    don't want to end up sorting index lists twice, I modified lib/indexes to
    remember if they have been sorted before. Hooray for dynamic objects.

--- testapp/views/lib/indexes.js
+++ testapp/views/lib/indexes.js
@@ -2,8 +2,14 @@
 // a list of indexes such as [2,4,5,6,8,11] into a presumably more
 // efficient pattern that reads 0-2-1-1-3-1-1-2-1.
 
-exports.compress=function(list) {
+exports.sort=function(list) {
+  if (list.sorted) return;
   list.sort(function(a,b) {return a-b;});
+  list.sorted=true;
+};
+
+exports.compress=function(list) {
+  exports.sort(list);
   var prefix=(list[0]?[0,list[0]]:[]);
   for (var index=0;index<list.length;index+=2) {
     var count=0, skip=0;
@@ -24,5 +30,8 @@ exports.decompress=function(string) {
     while (count--) list.push(index++);
     index+=parseInt(string.shift());
   }
+  // A decompressed list is always sorted; I set this
+  // merely out of principle and passion for consistency.
+  list.sorted=true;
   return list;
 };
