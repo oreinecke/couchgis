4e0c56474c0730c5f1187f14bbb174c1db7b9b95
Oscar Reinecke <reinecke@posteo.org>
2014-03-18 16:41:08 +0100

    Expanded _views/fields-values to flat field hierarchy.
    Fields with dots are quoted, as always. This works well with _list/tabulate, as
    long as no quotes are involved. Since I am expanding and contracting fields on
    multiple instances, this should go into a library.

--- testapp/views/fields-values/map.js
+++ testapp/views/fields-values/map.js
@@ -3,16 +3,24 @@
 function(doc) {
   if (doc.type==null) return;
   for (var field in doc) {
-    var val=doc[field];
     if (!/^[A-ZÄÖÜ]/.test(field)) continue;
     if (/^GeoJSON/.test(field)) continue;
-    // store detailed field info in summary
-    var summary={count:1,type:typeof(val)};
-    if (typeof(val)==="number") {
-      summary.min=val;
-      summary.max=val;
-      val=null;
-    }
-    emit([doc.type, field, val], summary);
+    (function flat_fields(obj, fields) {
+      if (obj && typeof obj==="object") {
+        for (var field in obj) {
+          var obj2=obj[field];
+          if (/\./.test(field)) field="'"+field+"'";
+          flat_fields(obj2, fields.concat([field]));
+        }
+        return;
+      }
+      var summary={count:1,type:typeof obj};
+      if (typeof obj==="number") {
+        summary.min=obj;
+        summary.max=obj;
+        obj=null;
+      }
+      emit([doc.type, fields.join('.'), obj], summary);
+    })(doc[field], [field]);
   }
 }
