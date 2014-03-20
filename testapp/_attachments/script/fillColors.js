d2a95f30a68307b30af24085efb3663ede6defda
Oscar Reinecke <reinecke@posteo.org>
2014-03-20 15:06:10 +0100

    Defined explicit fill colors in script/fillColors.js.
    Because as soon as I add a new type my beloved colors get screwed!!11!

---
+++ testapp/_attachments/script/fillColors.js
@@ -0,0 +1,7 @@
+// some nice colors for different types
+var fillColors=['#FF0000','#0000FF'];
+fillColors["Feldblock"]     = '#00FF80';
+fillColors["Fund"]          = '#8000FF';
+fillColors["Maßnahme"]      = '#80FF00';
+fillColors["Projektfläche"] = '#FF8000';
+fillColors["Vegetation"]    = '#0080FF';
