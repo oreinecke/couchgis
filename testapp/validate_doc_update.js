87477c14cad11b88e2b00667e506234116aa3252
Oscar Reinecke <reinecke@posteo.org>
2014-04-02 15:33:24 +0200

    Improved validate_doc_update to avoid identical revisions.
    JSON.stringify is probably not the best tool for this, but I very much enjoy
    the prospect of a clean document revision history from now on!!

--- testapp/validate_doc_update.js
+++ testapp/validate_doc_update.js
@@ -3,4 +3,10 @@ function(newDoc, oldDoc, userCtx, secObj) {
 // write-access for normal documents to all users.
   if (userCtx.roles.indexOf("user")===-1 && userCtx.roles.indexOf("_admin")===-1)
     throw({unauthorized: 'Database is read-only for non-members!'});
+  // Prevent database from writing an unchanged document (_revisions
+  // are taken out of the comparison because they differ all the time).
+  delete newDoc._revisions;
+  if (oldDoc) delete oldDoc._revisions;
+  if (JSON.stringify(newDoc)===JSON.stringify(oldDoc))
+    throw({forbidden: 'Uploaded document must differ from existing one.'});
 }
