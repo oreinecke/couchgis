afca5a620a4de4f366e3424f3f6585e9561bbc56
Oscar Reinecke <reinecke@posteo.org>
2014-04-29 13:33:37 +0200

    Fixed some ugly colors.

--- couchgis/_attachments/script/fillColors.js
+++ couchgis/_attachments/script/fillColors.js
@@ -1,7 +1,7 @@
 // some nice colors for different types
-var fillColors=['#FF0000','#0000FF'];
+var fillColors=['#FF0000','#FFFF00'];
 fillColors["Feldblock"]     = '#00FF80';
 fillColors["Fund"]          = '#8000FF';
 fillColors["Maßnahme"]      = '#80FF00';
-fillColors["Projektfläche"] = '#FF8000';
+fillColors["Probefläche"]   = '#FF8000';
 fillColors["Vegetation"]    = '#0080FF';
