1c7f74cc2141669b4b30e2d27042b17e67047e20
Oscar Reinecke <reinecke@posteo.org>
2014-05-05 13:28:54 +0200

    Excluded malign characters in the document id.

--- couchgis/validate_doc_update.js
+++ couchgis/validate_doc_update.js
@@ -5,6 +5,9 @@ function(newDoc, oldDoc, userCtx, secObj) {
     throw({unauthorized: 'Database is read-only for non-members.'});
   // Don't apply any restrictions to deleted docs.
   if (newDoc._deleted) return;
+  // Don't allow malign document ids with commas or spaces or anything.
+  if (newDoc._id.match(/[^\w-.]/))
+    throw({forbidden: "Document id must not contain '"+newDoc._id.match(/[^\w-.]+/)+"'."});
   // Prevent database from writing an unchanged document (_revisions
   // are taken out of the comparison because they differ all the time).
   delete newDoc._revisions;
