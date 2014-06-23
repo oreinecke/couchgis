650665e31c7363c79d64ce34ccc99bdf9c3b46e5
Oscar Reinecke <reinecke@posteo.org>
2014-06-23 15:12:00 +0200

    Protected relate function against unspecified geometry.

--- couchgis/lists/bbox-filter.js
+++ couchgis/lists/bbox-filter.js
@@ -89,7 +89,7 @@ function(head, req) {
     var related_GeoJSON_outside=relates;
     var related_Polygons=function(type, relation) {
       if (relation!=="intersects" && relation!=="contains") return;
-      relates=fail;
+      if (type) relates=fail;
       var inspect_types=["Polygon", "MultiPolygon"];
       if (inspect_types.indexOf(type)!==-1) return related_GeoJSON;
       if (type!=="GeometryCollection") return;
