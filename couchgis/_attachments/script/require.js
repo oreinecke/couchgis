db063b00d1801b4f3cb1df2ab4eb434182b27340
Oscar Reinecke <reinecke@posteo.org>
2014-07-31 14:14:29 +0200

    Rephrased explanation in script/require.js.

--- couchgis/_attachments/script/require.js
+++ couchgis/_attachments/script/require.js
@@ -1,7 +1,7 @@
 // Loads CommonJS libraries nested in CouchDB design docs using an identical
-// syntax. Requires inside libraries are evaluated and relative paths are
-// supported. Critical variables have been shadowed but the library would still
-// be able to manipulate other globals i.e. window, document, $ etc.
+// syntax. Calls to require within libraries are evaluated and relative paths
+// are supported. Critical variables have been shadowed but the library would
+// still be able to manipulate other globals i.e. window, document, $ etc.
 
 var require=function() {
   var ddoc;
