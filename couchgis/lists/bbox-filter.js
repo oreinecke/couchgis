b1dd5f8cfafa88c02a9541161e17a0c4d2cad4a8
Oscar Reinecke <reinecke@posteo.org>
2014-08-07 18:46:26 +0200

    Allowed for a slight wiggle room for bbox spatial relations.
    The tolerance is identical to the 5e-7 in utils/pointInPolygon and accounts for
    about 55mm.

--- couchgis/lists/bbox-filter.js
+++ couchgis/lists/bbox-filter.js
@@ -41,12 +41,16 @@ function(head, req) {
         bbox_2=bbox[2], bbox_3=bbox[3];
     switch(options.relation) {
     case "within":
+      bbox_0+=5e-7; bbox_2-=5e-7;
+      bbox_1+=5e-7; bbox_3-=5e-7;
       inside_bbox=function(bbox2) {
         return (bbox2[0]<=bbox_0&&bbox_2<=bbox2[2]&&
                 bbox2[1]<=bbox_1&&bbox_3<=bbox2[3]);
       };
       break;
     case "contains":
+      bbox_0-=5e-7; bbox_2+=5e-7;
+      bbox_1-=5e-7; bbox_3+=5e-7;
       inside_bbox=function(bbox2) {
         return (bbox_0<=bbox2[0]&&bbox2[2]<=bbox_2&&
                 bbox_1<=bbox2[1]&&bbox2[3]<=bbox_3);
