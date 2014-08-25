7971cf8c99b87733301746a5e758d02eb11129f6
Oscar Reinecke <reinecke@posteo.org>
2014-08-25 14:33:14 +0200

    Improved _view/types-year to use 'sensible granularity'.
    Previous tests showed that still way too many documents have to run by the
    post-filter because using no range as key if doc.time spans over more than one
    year just isn't restrictive enough.
    So I've come up with a smarter scheme that is still kind of intuitive:
    - Extract the very beginning and the very end from doc.time,
    - strip everything except for the year,
    - emit range if its a single year or a one-sided range, or
    - round to the next five years if doc.time spans over five years or less, and
      ditto for ten, fifty and a hundred years. For instance, "1960-1992" spans
      over 32 years, so a simplified range is "1950-2000".
    At the current data set, I end up with 48 unique keys of 3174 in total, and
    awesome performance, so job well done I guess.

--- couchgis/views/types-year/map.js
+++ couchgis/views/types-year/map.js
@@ -1,13 +1,22 @@
-// Sample [type, year] if doc.time is limited
-// to one year and [type, null] otherwhise.
+// Sample [type, year] at "reasonable granularity".
 
 function(doc) {
   var ranges=require('views/lib/ranges').toRanges(doc.time);
-  var range=ranges.pop(), year=range.begin[0];
-  if (year!==range.end[0]) year=null;
-  while (range=ranges.pop()) {
-    if (year!==range.begin[0]) year=null;
-    if (year!==range.end[0]) year=null;
+  var range=ranges.shift();
+  range.begin.splice(1);
+  if (ranges.length)
+    range.end=ranges.pop().end.slice(0,1);
+  else range.end.splice(1);
+  var granularity=(function(years) {
+    if (years>50) return 100;
+    if (years>10) return 50;
+    if (years>5) return 10;
+    if (years>1) return 5;
+  }(range.end[0]-range.begin[0]));
+  if (granularity) {
+    range.begin[0]-=range.begin[0]%granularity;
+    var remainder=range.end[0]%granularity;
+    if (remainder) range.end[0]+=granularity-remainder;
   }
-  emit([doc.type, year]);
+  emit([doc.type, [range]]);
 }
