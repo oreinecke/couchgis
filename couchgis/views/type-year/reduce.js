c7a9fbb98cb30c52127cee544f66ef22de894ecf
Oscar Reinecke <reinecke@posteo.org>
2014-08-26 15:56:59 +0200

    Improved reduce function of _view/type-year.
    It works now in conjunction with _list/tabulate because row.value has a valid
    document in it. This is neat because I can move the pre-filter key selection
    away from the map page, as it is just now.
    The value is set by the reduce function exclusively, because the map page still
    has to run through all these rows with reduce=false, and it isn't needed there
    at all.
    I wish I could reduce to a compiled range instead but this would
    require('views/lib/ranges') and those aren't allowed in a reduce function, so
    this is as good as it gets.

--- couchgis/views/type-year/reduce.js
+++ couchgis/views/type-year/reduce.js
@@ -1,1 +1,8 @@
-_count
+// For group_level=2, forward 1st of all identical keys as
+// document value that can be evaluated by _list/tabulate.
+
+function(keys, values, rereduce) {
+  if (rereduce) return values.pop();
+  var key=keys.pop()[0];
+  return {doc:{type:key[0], ranges:key[1] || undefined}};
+}
