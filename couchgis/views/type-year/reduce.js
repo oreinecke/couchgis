dabed9aa930c540c9f40f5ede314b33380986c65
Oscar Reinecke <reinecke@posteo.org>
2014-11-24 16:55:28 +0100

    Included union in _view/type-year.
    If queried with group=true, the value changes to an object with the property
    'one'. 'one' is a range-object that covers the time ranges of each document.
    So, if it happens to translate to "07/2009-06/2010" and I want to have
    documents from 2009-2010, I don't have to check each document individually.
    If I ask for documents of 06/2010, however, the documents of this key must be
    checked because there might be one from 09/2009 etc.

--- couchgis/views/type-year/reduce.js
+++ couchgis/views/type-year/reduce.js
@@ -1,8 +1,38 @@
-// For group_level=2, forward 1st of all identical keys as
-// document value that can be evaluated by _list/tabulate.
+// Find the smallest range that covers at least one
+// range, and the largest range that covers all ranges.
 
 function(keys, values, rereduce) {
-  if (rereduce) return values.pop();
-  var key=keys.pop()[0];
-  return {doc:{type:key[0], ranges:key[1] || undefined}};
+  for (var v=0; !rereduce && v<values.length; v++) {
+    var ranges=values[v].doc.ranges;
+    values[v]={
+      one: [{ begin:ranges[0].begin, end:ranges.pop().end }]
+    };
+  }
+  ;function lesser(a, b) {
+    for (var d=0;d<3;d++) {
+      if (d===a.length && d===b.length) return;
+      if (d===a.length) return a;
+      if (d===b.length) return b;
+      if (a[d]<b[d]) return a;
+      if (a[d]>b[d]) return b;
+    }
+  }
+  ;function greater(a, b) {
+    for (var d=0;d<3;d++) {
+      if (d===a.length && d===b.length) return;
+      if (d===a.length) return a;
+      if (d===b.length) return b;
+      if (a[d]>b[d]) return a;
+      if (a[d]<b[d]) return b;
+    }
+  }
+  }
+  var value=values.pop();
+  var one=value.one[0];
+  while ( value=values.pop() ) {
+    var one2=value.one[0];
+    one.begin=lesser(one.begin, one2.begin) || one.begin;
+    one.end=greater(one.end, one2.end) || one.end;
+  }
+  return { one:[one] };
 }
