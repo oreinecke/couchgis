608a845471e4cec3bf8b5ec6656310c9c820bff3
Oscar Reinecke <reinecke@posteo.org>
2014-11-26 00:18:52 +0100

    This simplifies local intersections() in _view/type-year.
    Making the world a better place one step at a time.

--- couchgis/views/type-year/reduce.js
+++ couchgis/views/type-year/reduce.js
@@ -33,17 +33,14 @@ function(keys, values, rereduce) {
     var b0=b.pop();
     var result=[];
     while (a0 && b0) {
-      var range={
-        begin:not(lesser, a0.begin, b0.begin),
-        end:not(greater, a0.end, b0.end)
-      };
-      if (function(a,b) {
-        for (var d=0;d<3;d++) {
-          if (d===a.length || d===b.length) return true;
-          if (a[d]<b[d]) return true;
-          if (a[d]>b[d]) return;
-        }
-      }(range.begin, range.end)) result.push(range);
+      var begin=not(lesser, a0.begin, b0.begin);
+      var end=not(greater, a0.end, b0.end);
+      for (var d=0;d<=3;d++) {
+        if (d===begin.length || d===end.length || begin[d]<end[d])
+          result.push({ begin:begin, end:end });
+        if (d===begin.length || d===end.length || begin[d]!==end[d])
+          break;
+      }
       var l=lesser(a0.begin, b0.begin);
       if (l!==a0.begin) a0=a.pop();
       if (l!==b0.begin) b0=b.pop();
