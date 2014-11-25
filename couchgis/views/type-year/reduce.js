7269841368c44fc146b76e4ce9b40b06f1555e39
Oscar Reinecke <reinecke@posteo.org>
2014-11-25 16:29:37 +0100

    Changed _view/type-year to compile range intersection.
    This is somehow quoted from ranges/intersects and sketches as such:
    - get a range from a and a range from b,
    - intersect ranges and ignore result, if it's empty,
    - exchange the range with what's next in its stack, if it's left end lies above
      the other range's left end.

--- couchgis/views/type-year/reduce.js
+++ couchgis/views/type-year/reduce.js
@@ -27,13 +27,37 @@ function(keys, values, rereduce) {
       if (a[d]<b[d]) return b;
     }
   }
+  ;function intersection(a, b) {
+    ;function not(relates, a, b) { return relates(a,b) === a ? b : a; }
+    var a0=a.pop();
+    var b0=b.pop();
+    var result=[];
+    while (a0 && b0) {
+      var range={
+        begin:not(lesser, a0.begin, b0.begin),
+        end:not(greater, a0.end, b0.end)
+      };
+      if (function(a,b) {
+        for (var d=0;d<3;d++) {
+          if (d===a.length || d===b.length) return true;
+          if (a[d]<b[d]) return true;
+          if (a[d]>b[d]) return;
+        }
+      }(range.begin, range.end)) result.push(range);
+      var l=lesser(a0.begin, b0.begin);
+      if (l!==a0.begin) a0=a.pop();
+      if (l!==b0.begin) b0=b.pop();
+    }
+    return result.reverse();
   }
   var value=values.pop();
   var one=value.one[0];
+  var all=value.all;
   while ( value=values.pop() ) {
     var one2=value.one[0];
     one.begin=lesser(one.begin, one2.begin) || one.begin;
     one.end=greater(one.end, one2.end) || one.end;
+    all=intersection(all, value.all);
   }
-  return { one:[one], all:[] };
+  return { one:[one], all:all };
 }
