28413f7fd213ffda83e4777bd19c641f54ef115f
Oscar Reinecke <reinecke@posteo.org>
2015-01-15 13:06:01 +0100

    Set _show/plaintext's Content-Type to text/plain.
    The default is text/html which makes no sense with all the spaces and whatnot.

--- couchgis/shows/plaintext.js
+++ couchgis/shows/plaintext.js
@@ -4,12 +4,15 @@
 function(doc, req) {
   delete doc.GeoJSON;
   delete doc._revisions;
-  return function stringify(obj, spaces, lines) {
-    for (var prop in obj)
-      if (obj[prop] && typeof obj[prop]==="object") {
-        lines.push(spaces+prop+':');
-        stringify(obj[prop], spaces+'  ', lines);
-      } else lines.push(spaces+prop+': '+obj[prop]);
-    return lines;
-  }(doc, "", []).join('\n');
+  return {
+    headers:{'Content-Type':'text/plain;charset=utf-8'},
+    body:function stringify(obj, spaces, lines) {
+      for (var prop in obj)
+        if (obj[prop] && typeof obj[prop]==="object") {
+          lines.push(spaces+prop+':');
+          stringify(obj[prop], spaces+'  ', lines);
+        } else lines.push(spaces+prop+': '+obj[prop]);
+      return lines;
+    }(doc, "", []).join('\n')
+  };
 }
