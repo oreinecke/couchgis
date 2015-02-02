610ef357c0a379ec1b37d1ae5ad5208cbdb804d6
Oscar Reinecke <reinecke@posteo.org>
2015-02-02 15:56:38 +0100

    Removed content from _view/field-type-value's reduce value.
    Because string content of about 2000 characters will produce a HTTP 500 'reduce
    overflow' error and the application won't even initialize.
    I've left doc.type for now since _list/tabulate uses it a couple of times.

--- couchgis/views/field-type-value/reduce.js
+++ couchgis/views/field-type-value/reduce.js
@@ -1,8 +1,6 @@
 // For group_level=2, forward 1st of all identical keys as
-// document value that can be evaluated by _list/tabulate.
+// typed document that can be evaluated by _list/tabulate.
 
-function reduce(keys, values, rereduce) {
-  if (rereduce) return values.pop();
-  var key=keys.pop()[0];
-  return { doc:{ type:key[1], content:key[2] } };
+function (keys, values, rereduce) {
+  return rereduce ? values[0] : { doc:{ type: keys[0][0][1] } };
 }
