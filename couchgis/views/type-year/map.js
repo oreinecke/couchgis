4d2a2d5d428c9034f7335c09c790e38bf1274698
Oscar Reinecke <reinecke@posteo.org>
2015-02-16 16:57:21 +0100

    Used ranges generator in _view/type-year.
    I replaced the single 'range' with var begin, end, and applied
    range().stringify to it.

--- couchgis/views/type-year/map.js
+++ couchgis/views/type-year/map.js
@@ -2,23 +2,20 @@
 
 function(doc) {
   if (!doc.type) return;
-  var ranges=require('views/lib/ranges');
-  var range=function(ranges) {
-    return {
-      begin:ranges[0].begin.slice(0,1),
-      end:ranges.pop().end.slice(0,1)
-    };
-  }(ranges.toRanges(doc.time));
+  var ranges=require('views/lib/ranges')(doc.time);
+  var begin=ranges[0].begin.slice(0,1);
+  var end=ranges.slice(-1)[0].end.slice(0,1);
   var granularity=(function(years) {
     if (years>50) return 100;
     if (years>10) return 50;
     if (years>5) return 10;
     if (years>1) return 5;
-  }(range.end[0]-range.begin[0]));
+  }(end[0]-begin[0]));
   if (granularity) {
-    range.begin[0]-=range.begin[0]%granularity;
-    var remainder=range.end[0]%granularity;
-    if (remainder) range.end[0]+=granularity-remainder;
+    begin[0]-=begin[0]%granularity;
+    var remainder=end[0]%granularity;
+    if (remainder) end[0]+=granularity-remainder;
   }
-  emit([doc.type, ranges.toString([range])], {doc:{type:doc.type, ranges:ranges.toRanges(doc.time)}});
+  var year=ranges.toString.apply([{begin:begin, end:end}]);
+  emit([doc.type, year], {doc:{type:doc.type, ranges:ranges}});
 }
