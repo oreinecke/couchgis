7f87cff49612d12d942600a11854403fe3cc2ec3
Oscar Reinecke <reinecke@posteo.org>
2015-03-09 17:01:50 +0100

    Simplified _view/size's emit value.

--- couchgis/views/size/map.js
+++ couchgis/views/size/map.js
@@ -1,19 +1,12 @@
-// Emit size as key and bbox for reduce.
+// Emit size as key and tag hidden documents.
 
 function(doc) {
   var utils=require('views/lib/utils');
   var ranges=require('views/lib/ranges')(doc.time);
-  if (doc.GeoJSON==null) {
-    emit(null, {ranges:ranges});
-    return;
-  }
+  if (!doc.GeoJSON) return;
   var GeoJSON=utils.clone(doc.GeoJSON);
   utils.toWGS84(GeoJSON);
   utils.bbox(GeoJSON);
   utils.size(GeoJSON);
-  emit(GeoJSON.size, {
-    deleted: !doc.type || undefined,
-    bbox:GeoJSON.bbox,
-    ranges:ranges
-  });
+  emit(GeoJSON.size, {deleted: !doc.type || undefined});
 }
