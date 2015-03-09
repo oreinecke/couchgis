062042b79499c320461ba26200db4ee0d616637c
Oscar Reinecke <reinecke@posteo.org>
2015-03-09 13:39:38 +0100

    Skipped rows that link to itself in _view/geojson-other-doc.

--- couchgis/views/geojson-other-doc/map.js
+++ couchgis/views/geojson-other-doc/map.js
@@ -2,9 +2,10 @@
 
 function(doc) {
   if (!doc.type) return;
-  var ids=doc[ doc.GeoJSON ? "_id" : "GeoJSON_clone" ];
+  var ids=doc[ doc.GeoJSON ? "_id" : "GeoJSON_clone" ], id;
   if (!ids) return;
   ids=ids.replace(/[#%].*($)/gm, '$1');
   ids=ids.match(/[^\s,;&]+/g);
-  while (ids.length) emit(ids.shift());
+  while ( id=ids.pop() )
+    if (id!==doc._id) emit(id);
 }
