6696be2acfa3d4571920f846c70b6027ac8486ef
Oscar Reinecke <reinecke@posteo.org>
2015-03-09 16:58:37 +0100

    Copied _view/dimensions to _view/size.
    _view/size will aim for a smaller value because currently 1.8MB need to be
    fetched.

---
+++ couchgis/views/size/map.js
@@ -0,0 +1,19 @@
+// Emit size as key and bbox for reduce.
+
+function(doc) {
+  var utils=require('views/lib/utils');
+  var ranges=require('views/lib/ranges')(doc.time);
+  if (doc.GeoJSON==null) {
+    emit(null, {ranges:ranges});
+    return;
+  }
+  var GeoJSON=utils.clone(doc.GeoJSON);
+  utils.toWGS84(GeoJSON);
+  utils.bbox(GeoJSON);
+  utils.size(GeoJSON);
+  emit(GeoJSON.size, {
+    deleted: !doc.type || undefined,
+    bbox:GeoJSON.bbox,
+    ranges:ranges
+  });
+}
