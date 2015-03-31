3870b6e6a1f23344d177fd710fdd1191c02041d5
Oscar Reinecke <reinecke@posteo.org>
2015-03-31 15:35:17 +0200

    Factored out common  match expression in lib/path.

--- couchgis/views/lib/path.js
+++ couchgis/views/lib/path.js
@@ -2,8 +2,10 @@
 
 // "' OBJ.' . objects's field" -> [" OBJ.","object's field"]
 
+var match_parts=/\s*'[^']+'\s*|[^.]+/g;
+
 exports.decode=function(path) {
-  var result=path.match(/\s*'[^']+'\s*|[^.]+/g);
+  var result=path.match(match_parts);
   for (var r=0;r<result.length;r++)
     result[r]=result[r].replace(/^\s+|\s+$/g,"").replace(/^'|'$/g,"");
   return result;
@@ -22,5 +24,5 @@ exports.encode=function(fields) {
 
 // Pretty-print path with extra characters after the dot.
 exports.pretty=function(path, extra) {
-  return path.match(/\s*'[^']+'\s*|[^.]+/g).join('.'+extra);
+  return path.match(match_parts).join('.'+extra);
 }
