3a1fac50b1eeb0f261d3adae3abef8d45f1644e3
Oscar Reinecke <reinecke@posteo.org>
2015-04-09 18:47:33 +0200

    This allows me to use _list/export/field-type-value.

--- couchgis/views/field-type-value/reduce.js
+++ couchgis/views/field-type-value/reduce.js
@@ -2,5 +2,11 @@
 // typed document that can be evaluated by _list/tabulate.
 
 function (keys, values, rereduce) {
-  return rereduce ? values[0] : { doc:{ type: keys[0][0][1] } };
+  if (rereduce) return values[0];
+  var value=keys[0][0][2];
+  if (typeof value==="string" && value.length>200)
+    value=value.slice(0,196)+" ...";
+  var doc={ type:keys[0][0][1] };
+  doc[keys[0][0][0]]=value;
+  return { doc:doc };
 }
